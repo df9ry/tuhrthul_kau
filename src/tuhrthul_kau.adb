ba4207d699940f7316a6e0e092af44316d58fd9a
Reiner / Tania Hagn <reiner@jemira.net>
2024-05-10 19:55:41 +0200

    Initial commit

---
+++ src/tuhrthul_kau.adb
@@ -0,0 +1,36 @@
+--  predefined units of the library
+with Gtk.Main;
+with Gtk.Enums;
+with Gtk.Window;
+
+--  My units
+--  with Callbacks;
+
+procedure Tuhrthul_Kau is
+
+   procedure Create_Window is
+      Main_Window : Gtk.Window.Gtk_Window;
+   begin
+      Gtk.Window.Gtk_New
+        (Window   => Main_Window,
+         The_Type => Gtk.Enums.Window_Toplevel);
+
+      --  From Gtk.Widget:
+      Gtk.Window.Set_Title (Window => Main_Window, Title  => "Tuhrthul Kau");
+
+      --  Construct the window and connect various callbacks
+
+      --  ...
+      Gtk.Window.Show_All (Main_Window);
+   end Create_Window;
+
+begin --  Tuhrtuhl_Kau
+   --  Initializes GtkAda
+   Gtk.Main.Init;
+
+   --  Create the main window
+   Create_Window;
+
+   --  Signal handling loop
+   Gtk.Main.Main;
+end Tuhrthul_Kau;
